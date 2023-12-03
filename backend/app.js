a49974267696ecc8efd0279f2e560426c0f44c9a
flaamdeen <panditmukki5@gmail.com>
2023-12-03 17:11:45 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,9 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000"],
     credentials: true,
+    optionsSuccessStatus: 200,
   })
 );
 
