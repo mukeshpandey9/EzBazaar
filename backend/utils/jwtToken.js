ceef4cc6ba0cdc7a8ee1a5af9e629b934d38c5b9
flaamdeen <panditmukki5@gmail.com>
2023-12-03 17:11:45 +0530

    updated

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -10,6 +10,7 @@ const sendToken = (user, statusCode, res) => {
       expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
 
       httpOnly: true,
+      domain: "http://localhost:3000",
     };
 
     res.status(statusCode).cookie("token", token, options).json({
